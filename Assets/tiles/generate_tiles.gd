cd34971af5344242525e0589468bf75404199ffb
Aidan-Reese <aidanreese.professional@gmail.com>
2025-07-22 13:19:23 -0600

    Tile map generated

--- Assets/tiles/generate_tiles.gd
+++ Assets/tiles/generate_tiles.gd
@@ -1,25 +1,15 @@
 extends Node
-
-var tile_colors = {
-	"highway": "#303030",
-	"road": "#606060",
-	"trail": "#CB8664",
-	"desert": "#F6D0B0",
-	"plains": "#3F5D4B",
-	"forest": "#2C412E",
-	"swamp": "#2A4B46",
-	"mountains": "#273833",
-	"impassable": "#142C55",
-	"marked": "#9900FF"
+var settlement_colors = {
+	   "near_impassible": "#0F2227" 
 }
 
 var size = Vector2i(24, 24)
 
 func _ready():
-	for name in tile_colors.keys():
+	for name in settlement_colors.keys():
 		var img = Image.create(size.x, size.y, false, Image.FORMAT_RGBA8)
-		img.fill(Color(tile_colors[name]))
+		img.fill(Color(settlement_colors[name]))
 		var path = "res://Assets/tiles/" + name + ".png"
 		img.save_png(path)
 		print("Saved: ", path)
-	print("All tiles generated!")
+	print("All settlement tiles generated!")
