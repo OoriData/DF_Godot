01e3eba5cacd4c903387df7c6f2921d7ffe7f491
Aidan-Reese <aidanreese.professional@gmail.com>
2025-08-22 13:15:07 -0600

    Prod URL change

--- Scripts/System/api_calls.gd
+++ Scripts/System/api_calls.gd
@@ -34,7 +34,8 @@ signal route_choices_received(routes: Array)
 signal convoy_sent_on_journey(updated_convoy_data: Dictionary)
 signal convoy_journey_canceled(updated_convoy_data: Dictionary)
 
-var BASE_URL: String = 'http://127.0.0.1:1337' # default
+# var BASE_URL: String = 'http://127.0.0.1:1337' # default
+var BASE_URL: String = 'https://df-api.oori.dev:1337/' # default
 # Allow override via environment (cannot be const due to runtime lookup)
 func _init():
 	if OS.has_environment('DF_API_BASE_URL'):
